00c28a64934c099dd206349754ef64e8bf5f9051
Sqntos-tech <edwardsantos1237@gmail.com>
2026-07-08 22:10:30 -0400

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('beautiful')
+console.log('Hello World'[10])
+console.log(''.length)
+let str = 'Edward'
+console.log(str[str.length - 1])
+
+let fullName = 'Edward Santos'
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32;
+console.log('fahrenheit')
+let bool = '1' === 1
+console.log(bool)
+
+console.log('edwarddaty') 
