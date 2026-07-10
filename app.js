15ae711f91f974eab449b219252b99bbccdeb124
Sqntos-tech <edwardsantos1237@gmail.com>
2026-07-10 18:38:35 -0400

    update

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+/* NOTES */
+
 console.log('beautiful')
 console.log('Hello World'[10])
 console.log(''.length)
@@ -11,4 +14,67 @@ console.log('fahrenheit')
 let bool = '1' === 1
 console.log(bool)
 
-console.log('edwarddaty') 
+console.log('edwarddaty')
+
+{let cash = 60
+let price = 40
+let difference = cash - price
+
+if (cash > price) {
+    console.log(`you paid extra - here's ${difference} dollars change`)
+}
+else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day!")
+}
+else {
+    console.log(`not enough money - you still owe ${price-cash} dollars`)
+}
+}
+
+{let cash = 50
+let price = 40
+let isStoreOpen = false
+
+if (cash >= price && isStoreOpen === false) {
+    console.log('print the receipt')
+}                     (!isStoreOpen)}
+
+let cash = 50 
+let price = 40
+let isStoreOpen = true
+
+{let str = cash >= price && isStoreOpen ? "give receipt" : "do not give receipt"
+console.log(str)}
+
+// let count = 1
+
+// while (count <= 5) {
+//     console.log(count)
+//     count = count + 1
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+for (let i = 1; i < 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if (i % 3 === 0) {
+        console.log(` ${i} -> Frontend`)
+    }
+    else if (i % 5 === 0) {
+        console.log(`${i} -> Simplified`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
+}
+
+{const str = "Frontend Simplified"
+
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
+} }
